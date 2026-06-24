f415554b04fcd3cd3dd31eadfc791f40d3fc9f7c
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-24 12:30:05 +0530

    hello world

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Please enter your username::"
-read USER_Name # here USER_NAME is variable , whatever you enter in terminal stores in this vatiable
+read USER_NAME # here USER_NAME is variable , whatever you enter in terminal stores in this vatiable
 echo "userame is  $USER_NAME"
