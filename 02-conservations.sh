6bc4c10d29004474231723d3da8c594271c9a872
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-24 12:09:41 +0530

    hello world

--- 02-conservations.sh
+++ 02-conservations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "Trump : hey iran, we are going to blast you!"
-echo "Itan : yes , please , you are welcome"
-echo "Trump : hey it=ran iam serious"
-echo "Iran : hey Trump, we are also serious"
+
+Person1=Trump
+Person2=Iran
+echo "$Person1 : hey iran, we are going to blast you!"
+echo "$Person2 : yes , please , you are welcome"
+echo "$Person1 : hey iran iam serious"
+echo "$Person2 : hey Trump, we are also serious"
