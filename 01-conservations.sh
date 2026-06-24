6238953b6c383dfb6e370b4971d9bcad4555ede2
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-24 11:48:52 +0530

    hello world

---
+++ 01-conservations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Trump : hey iran, we are going to blast you!"
+echo "Itan : yes , please , you are welcome"
+echo "Trump : hey it=ran iam serious"
+echo "Iran : hey Trump, we are also serious"
