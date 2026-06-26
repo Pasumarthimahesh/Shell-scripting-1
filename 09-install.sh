18f9a5486284182b3768f722e01657c6b4ad5559
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-26 14:12:27 +0530

    hello world

---
+++ 09-install.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+ USERID=$(id -u)
+
+ #check root user or not
+
+ if [ $USERID -ne 0 ]; then
+ echo "please run this script with root cause"
+ exit 1
+ fi
+
+ echo " i am continuing.."
