5ca194d4eb0b8a0dc5d2ada690bb6c6a658fee1b
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-26 13:32:51 +0530

    hello world

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -10,6 +10,6 @@ NUMBER=$1
 #le -less than or equal
 
 
-if [$NUMBER -ge 20]; then
-echo "Given Number $NUMBER is greater thann or equsl to 20"
+if [ $NUMBER -ge 20 ]; then
+echo "Given Number $NUMBER is greater thann or equal to 20"
 fi
