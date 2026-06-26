48052fb2006f177edd74b38d8017acfb4aec6cf7
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-26 13:27:36 +0530

    hello world

---
+++ 08-Conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+#gt -greater than
+#lt -lessthan
+#eq -equal 
+#ne-not equal
+#ge -greater thanor equal to 
+#le -less than or equal 
+
+
+if [$NUMBER -gt 20];then
+echo"Given Number $NUMBER is greater thann or equsl to 20"
+fi
