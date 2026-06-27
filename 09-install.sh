a9e928b85e7448a0657ee4fd2799d8e9ff14b14f
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-27 17:31:13 +0530

    Hellow world

--- 09-install.sh
+++ 09-install.sh
@@ -1,27 +1,11 @@
- #!/bin/bash
+#!/bin/bash
 
- USERID=$(id -u)
+USERID=$(id -u)
 
- #check root user or not
+#check root access or not
 
- if [ $USERID -ne 0 ]; then
- echo "please run this script with root cause"
- exit 1
- fi
-
- # echo " i am continuing.."
-dnf list installed mysql
-
-if [ $? -eq 0 ]; then 
-echo "Mysql is already installed ... SKIPPING"
-else
- echo "installing Mysql"
- dnf install mysql -y
-
-if [ $? -ne 0 ]; then 
-echo "Installing MYSQL is ...Failed"
-exit 1
-else 
-echo "Installing Mysql is ...SUCESS"
-fi 
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root access"
 fi
+
+echo  "i am continuing....."
