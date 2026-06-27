0b80cf6ddd6371ff6d15431915628ff5b13e6bd5
PasumarthiMahesh <Pasumarthimahesh7799@gmail.com>
2026-06-27 18:17:02 +0530

    Hellow world

--- 09-install.sh
+++ 09-install.sh
@@ -9,6 +9,23 @@ echo "Please run this script with root access"
 exit 1
 fi
 
+
+#first arg - what are you trying to install
+#second arg ->exit code
+
+VALIDATE(){
+
+ if [ $2 -ne 0 ]; then
+            echo "Installing $1 .......failed" 
+            exit 1 
+        else
+            echo "Installing $1 is ...SUCESS"
+        fi
+
+
+}
+
+
 # echo  "i am continuing....."
 
 dnf list installed mysql
@@ -16,12 +33,18 @@ dnf list installed mysql
 if  [ $? -eq 0 ]; then
 echo " mysql is already installed ...SKIPPING"
 else
-   echo "installing MYSQL"
-     dnf install mysql -y
-     if [ $? -ne 0 ]; then
-     echo "Installing MYSQL .......failed" 
-    exit 1 
-     else
-     echo "Installing MYSQL is ...SUCESS"
-     fi
+        echo "installing MYSQL"
+        dnf install mysql -y
+        VALIDATE MYSQL $?
+fi
+
+# echo  "i am continuing....."
+
+dnf list installed nginx
+if  [ $? -eq 0 ]; then
+echo " nginx is already installed ...SKIPPING"
+else
+        echo "installing nginx"
+        dnf install nginx -y
+       VALIDATE nginx $?
 fi
